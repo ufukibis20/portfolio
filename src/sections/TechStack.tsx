ed36fcdc77ab18521a771077370fb29c295ddd98
ufo401 <ufuk-ibis@live.de>
2026-03-08 17:34:59 +0100

    update

--- src/sections/TechStack.tsx
+++ src/sections/TechStack.tsx
@@ -31,8 +31,8 @@ function TechStack() {
       icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nodejs/nodejs-original.svg",
     },
     {
-      name: "Git",
-      icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/git/git-original.svg",
+      name: "GitHub",
+      icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original.svg",
     },
     {
       name: "REST APIs",
